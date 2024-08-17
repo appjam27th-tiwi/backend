33b309285f3b1a5718b4aadd218167f0ad8e8b6b
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-08-18 00:34:27 +0900

    fix: api key

--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -1,5 +1,5 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  GEMINI_API_KEY: Joi.string().required().hostname(),
+  GEMINI_API_KEY: Joi.string().required()
 });
--- src/gemini/enum/location.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum LocationEnum {
-	HOTEL = 'hotel',
-	FOOD = 'food',
-	SHOPPING = 'shopping',
-	SIGHTSEEING = 'sightseeing',
-}
