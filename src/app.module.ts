d7509a72ce806e46102fc791fb7ae27cda0cedc2
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-08-17 21:44:38 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,15 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './config/validationSchema';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 
 @Module({
   imports: [
-    UsersModule,
     ConfigModule.forRoot({
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       isGlobal: true,
       validationSchema,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: process.env.DATABASE_HOST,
-      port: 3306,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
-    }),
-    AuthModule
   ],
 })
 export class AppModule {}
