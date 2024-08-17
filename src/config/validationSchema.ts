4bf152ea2976ae7a44dc6370638d36c9de4403fc
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-08-17 21:49:28 +0900

    fix: validation scheme

--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -1,9 +1,5 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  DATABASE_HOST: Joi.string().required().hostname(),
-  DATABASE_USERNAME: Joi.string().required(),
-  DATABASE_PASSWORD: Joi.string().required(),
-  DATABASE_NAME: Joi.string().required(),
-  DATABASE_SYNCHRONIZE: Joi.boolean().required(),
+  GEMINI_API_KEY: Joi.string().required().hostname(),
 });
