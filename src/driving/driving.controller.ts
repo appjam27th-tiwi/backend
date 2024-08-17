9b0a7608f9102ae27801d61ef1302b814b45168c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-08-18 02:59:26 +0900

    fix: proxy last

--- src/driving/driving.controller.ts
+++ src/driving/driving.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+import axios from 'axios';
 
 @Controller('driving')
 export class DrivingController {
@@ -14,15 +16,20 @@ export class DrivingController {
 	async driving(
 		@Query('start') start: string,
 		@Query('goal') goal: string,
-		@Query('waypoints') waypoints: string
+		@Query('waypoints') waypoints: string,
+		@Res() res: Response
 	) {
 		try {
-			return this.httpService.get(`https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${ start }&goal=${ goal }&waypoints=${ waypoints }`, {
+			const result = await axios.get(`https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${ start }&goal=${ goal }&waypoints=${ waypoints }`, {
 				headers: {
 					'X-NCP-APIGW-API-KEY-ID': this.configService.get('NAVER_CLIENT_ID'),
 					'X-NCP-APIGW-API-KEY': this.configService.get('NAVER_CLIENT_SECRET'),
 				},
 			})
+
+			console.log(result.data);
+
+			return res.json(result.data);
 		} catch (error) {
 			console.error(error);
 			return {
