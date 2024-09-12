bcb56e06cf2aba2303c60438583885ae0c45f0b9
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-09-13 08:28:08 +0900

    semi final

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
